b618126ddd42b68ec106e4da0a7cc2af908cad86
Codaaman <coderaman30@gmail.com>
2026-09-09 16:32:57 +0530

    Clean up data_0.2.py by removing comments and blanks
    Removed commented-out code and unnecessary blank lines.

--- data_0.2.py
+++ data_0.2.py
@@ -1,12 +1,7 @@
 import pandas as pd
 import numpy as np
 
-
-
-
-
 df=pd.read_csv(r"D:\Torch_frame_work\clean_data.csv")
-
 
 
 d1=[]
@@ -14,22 +9,14 @@ d2=[]
 
 
 
-
 for k in df.iloc[:5000,1]:
     d1.append(k)
-    #d1["text"]=k
 
 
-# for i in df.iloc[:250,1]:
-#     d1.append(i)
-    #d1["text"]=k
 
 for k in df.iloc[:5000,2]:
 
     d2.append(k)
-
-# for i in df.iloc[:250,2]:
-#     d2.append(i)
 
 
 for k in df.iloc[57011:61374,1]:
@@ -61,19 +48,6 @@ for k in df.iloc[40000:40319,2]:
 
 
 
-
-
-
-
-
-
-
-
-# d2["label"]=df.iloc[:250,2]
-# d2["label"]=df.iloc[2500:2750,2]
-
-
-#d3=d1.update(d2)
 df=pd.DataFrame({"text":d1,"label":d2})
 
 
